35f8914e9b30bf82b401045076a6682d64d81e49
Will C <will@quid.works>
2019-10-21 21:09:38 -0400

    Scaffold for accepting clicks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Grid from './Grid';
 
-// function generateBoard(difficulty) {
-//   s
-// }
+import { useImmerReducer } from 'use-immer';
+
+const initialState = {
+  grid: [],
+}
+
+function reducer(draft, action) {
+  switch (action.type) {
+    case 'CLICK_TARGET':
+      console.log(action)
+      draft.grid[action.row][action.col] = action.value
+      break;
+    case 'HYDRATE_GRID':
+      draft.grid = action.grid
+      break;
+    default:
+      return draft;
+  }
+}
+
+const set = new Set(['h', 'x', 'b', '1', '2', '3', '4', '5', '6', '7', '8'])
 
 function App() {
+  const [state, dispatch] = useImmerReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({
+      type: 'HYDRATE_GRID',
+      grid: [
+        ['h', 'h', 'h', 'h', 'h'],
+        ['h', 'h', 'h', 'h', 'h'],
+        ['h', 'h', 'h', 'h', 'h'],
+        ['h', 'h', 'h', 'h', 'h'],
+        ['h', 'h', 'h', 'h', 'h'],
+      ]
+    })
+  }, [dispatch])
+
+  function handleClick(row, col) {
+    const index = Math.round(Math.random() * set.size)
+    const value = Array.from(set)[index]
+    // do async stuff...
+
+    dispatch({
+      type: 'CLICK_TARGET',
+      value,
+      row,
+      col,
+    })
+  }
+
   return (
     <div className="App">
       <h1>Minesweeper</h1>
-      <Grid row={7} col={7} />
+      <Grid grid={state.grid} onClick={handleClick} />
     </div>
   );
 }
--- src/Grid.js
+++ src/Grid.js
@@ -1,40 +1,75 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './Grid.css'
-import { useImmer } from 'use-immer';
 
-export default function ({ row, col }) {
-  const [state, update] = useImmer({
-    grid: []
-  })
+function CellContent({ fill }) {
+  let render
+  switch (fill) {
+    case 'x':
+      render = 'bomb'
+      break;
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+      render = fill
+      break;
+    case 'b':
+    case 'h':
+    default:
+      render = null
+  }
 
-  useEffect(() => {
-    const grid = []
-    console.log(row, col)
-    for (let i = 0; i < row; i++) {
-      console.log(grid)
-      grid.push([])
-      for (let j = 0; j < col; j++) {
-        console.log('cell', i, j)
-        grid[i].push('B')
+  return render
+}
+
+export default function ({ grid, row, col, onClick }) {
+  // const [state, update] = useImmer({
+  //   grid: []
+  // })
+
+  // useEffect(() => {
+  //   const grid = []
+  //   console.log(row, col)
+  //   for (let i = 0; i < row; i++) {
+  //     console.log(grid)
+  //     grid.push([])
+  //     for (let j = 0; j < col; j++) {
+  //       console.log('cell', i, j)
+  //       grid[i].push('B')
+  //     }
+  //   }
+
+  //   console.log(grid)
+  //   update(draft => {
+  //     draft.grid = grid
+  //   })
+  // }, [col, row, update])
+
+  // console.log(state)
+  // return <div className="wrapper">
+  //   {
+  //     state.grid.map((row, i) => {
+  //       return <div className="row">
+  //         {row.map((cell, j) => {
+  //           return <div key={i + ':' + j} className="cell"></div>
+  //         })}
+  //       </div>
+  //     })
+  //   }
+  // </div >
+  return grid.map((row, i) => {
+    return <div key={i} className="row">
+      {
+        row.map((cell, j) => {
+          return <div key={i + ':' + j} className={`cell cell-${cell}`} onClick={() => onClick(i, j)} >
+            <CellContent fill={cell} />
+          </div >
+        })
       }
-    }
-
-    console.log(grid)
-    update(draft => {
-      draft.grid = grid
-    })
-  }, [col, row, update])
-
-  console.log(state)
-  return <div className="wrapper">
-    {
-      state.grid.map((row, i) => {
-        return <div className="row">
-          {row.map((cell, j) => {
-            return <div key={i + ':' + j} className="cell"></div>
-          })}
-        </div>
-      })
-    }
-  </div >
+    </div>
+  })
 }
