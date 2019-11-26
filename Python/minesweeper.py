86667441018780b509fef34a69473455b9f4636f
Will C <will@quid.works>
2019-11-25 21:45:39 -0500

    Generate board working.

--- Python/minesweeper.py
+++ Python/minesweeper.py
@@ -10,6 +10,24 @@ class Game:
     self.numMines = 50
 
   def startNewGame(self):
+    self._generateBoard()
+
+  # On every checkCell call, return new game state.
+  def checkCell(self, row, col):
+    if self.hasFirstClick == False:
+      # populate board
+      self._populateBoard()
+    elif self.grid[row][col] == "M":
+      self.status = "lose"
+      self.grid[row][col] = "X"
+      return
+
+    numUpdates = self._updateBoard(row, col)
+    self.unvisitedCount -= numUpdates
+    if unvisitedCount == 0:
+      self.status = "win"
+
+  def _generateBoard(self):
     self.status = "playing"
     self.hasFirstClick = False
     grid = [[]] * self.H
@@ -17,6 +35,9 @@ class Game:
       grid[i] = ["U"] * self.W
     self.grid = grid
 
+  def _populateBoard(self, protectedRow, protectedCol):
+
+  def _updateBoard(self, row, col):
 
 
 game = Game()
