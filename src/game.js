47a2052efa278d0f9caf61c61d28256ebbc2e2a8
Will C <will@quid.works>
2019-11-30 21:52:45 -0500

    Separate game from cli.

--- src/game.js
+++ src/game.js
@@ -1,8 +1,3 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
 class Board {
   constructor(W, H, numMines) {
     this.W = W
@@ -198,51 +193,5 @@ class Game {
     })
   }
 }
-(async function main() {
 
-  const game = new Game()
-
-  game.start()
-  game.subscribe((grid) => {
-    // print board on each broadcast
-    console.log('Board:')
-    let outputTopRow = '  '
-    grid[0].forEach((col, j) => {
-      outputTopRow += String(j) + ' '
-    })
-    console.log(outputTopRow)
-    grid.forEach((row, i) => {
-      let output = '' + i + ' '
-      row.forEach((col) => {
-        let filter = col === 'M' || col === 'U' ? '?' : col
-        output += `${filter} `
-      })
-      output += ' ' + i
-      console.log(output)
-    })
-    console.log(outputTopRow)
-  })
-
-  console.log('Starting a new game of minesweeper')
-
-  function prompUser() {
-    return new Promise((resolve, reject) => {
-      readline.question(`Pick a square(separte with a space):`, (input) => {
-        const [row, col] = input.split(' ')
-        game.click(Number(row), Number(col))
-        resolve()
-      });
-    })
-  }
-
-  while (game.status === 'playing') {
-    await prompUser()
-  }
-
-  if (game.status === 'wins') {
-    console.log('You win!')
-  } else {
-    console.log('You lose!')
-  }
-  readline.close()
-})()
+module.exports = Game
